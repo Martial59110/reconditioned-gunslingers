a57379a4fe2329644eecc99f9fcd7344708705c6
julienbeauchant <julienbeauchant.pro@yahoo.com>
2024-11-22 10:03:38 +0100

    docs(bdd/): update bdd-init.sql

--- doc/bdd/bdd-init.sql
+++ doc/bdd/bdd-init.sql
@@ -5,6 +5,12 @@
 psql postgres 
 
 --------------------------------------------------------------------------------------------------------
+-- Suppression de la base de données simpluedo si elle existe déjà : 
+--------------------------------------------------------------------------------------------------------
+
+DROP DATABASE simpluedo;
+
+--------------------------------------------------------------------------------------------------------
 -- Création de la base de données simpluedo : 
 --------------------------------------------------------------------------------------------------------
 
@@ -20,7 +26,7 @@ CREATE USER admin_simpluedo WITH PASSWORD 'password_simpluedo';
 -- Accord du privilège de création de rôle à l'utilisateur admin_simpluedo : 
 --------------------------------------------------------------------------------------------------------
 
-ALTER ROLE admin_simpluedo WITH CREATEROLE;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 --------------------------------------------------------------------------------------------------------
 -- Quitter l'interface psql : 
@@ -32,13 +38,13 @@ ALTER ROLE admin_simpluedo WITH CREATEROLE;
 -- Connexion à la base de données simpluedo avec l'identifiant admin_simpluedo en utilisant pgcli :
 --------------------------------------------------------------------------------------------------------
 
-pgcli -U admin_simpluedo -d simpluedo
+pgcli -U postgres -d simpluedo
 
 --------------------------------------------------------------------------------------------------------
 -- Création de la table role : 
 --------------------------------------------------------------------------------------------------------
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id_role INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     nom_role VARCHAR(50) NOT NULL,
 );
@@ -47,7 +53,7 @@ CREATE TABLE role (
 -- Création de la table personnage :
 --------------------------------------------------------------------------------------------------------
 
-CREATE TABLE personnage (
+CREATE TABLE personnages (
     id_personnage INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     nom_personnage VARCHAR(50) NOT NULL,
 );
@@ -56,20 +62,20 @@ CREATE TABLE personnage (
 -- Création de la table utilisateur : 
 --------------------------------------------------------------------------------------------------------
 
-CREATE TABLE utilisateur (
-    uuid_utilisateur UUID PRIMARY KEY,
+CREATE TABLE utilisateurs (
+    uuid_utilisateur UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     pseudo_utilisateur VARCHAR(50) NOT NULL,
     id_role INTEGER NOT NULL,
     id_personnage INTEGER NOT NULL,
-    FOREIGN KEY (id_role) REFERENCES role (id_role),
-    FOREIGN KEY (id_personnage) REFERENCES personnage (id_personnage),
+    FOREIGN KEY (id_role) REFERENCES role (id_role) ON UPDATE CASCADE,
+    FOREIGN KEY (id_personnage) REFERENCES personnage (id_personnage) ON UPDATE CASCADE,
 );
 
 --------------------------------------------------------------------------------------------------------
 -- Création de la table salle :
 --------------------------------------------------------------------------------------------------------
 
-CREATE TABLE salle (
+CREATE TABLE salles (
     id_salle INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     nom_salle VARCHAR(50) NOT NULL,
 );
@@ -78,11 +84,11 @@ CREATE TABLE salle (
 -- Création de la table objet :
 --------------------------------------------------------------------------------------------------------
 
-CREATE TABLE objet (
+CREATE TABLE objets (
     id_objet INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     nom_objet VARCHAR(50) NOT NULL,
     id_salle INTEGER NOT NULL,
-    FOREIGN KEY (id_salle) REFERENCES salle (id_salle),
+    FOREIGN KEY (id_salle) REFERENCES salle (id_salle) ON UPDATE CASCADE,
 );
 
 --------------------------------------------------------------------------------------------------------
@@ -90,15 +96,24 @@ CREATE TABLE objet (
 --------------------------------------------------------------------------------------------------------
 
 CREATE TABLE visiter (
-    id_visiter INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    heure_arrivee TIMESTAMPZ NOT NULL,
-    heure_sortie TIMESTAMPZ NOT NULL,
-    id_personnage INTEGER NOT NULL,
-    id_salle INTEGER NOT NULL,
-    FOREIGN KEY (id_personnage) REFERENCES personnage (id_personnage),
-    FOREIGN KEY (id_salle) REFERENCES salle (id_salle),
+    id_personnage INTEGER REFERENCES personnage(id_personnage),
+    id_salle INTEGER REFERENCES salle(id_salle),
+    heure_arrivee TIME NOT NULL,
+    heure_sortie TIME NULL,
+    PRIMARY KEY(id_personnage, id_salle, heure_arrivee)
 );
 
+--------------------------------------------------------------------------------------------------------
+-- Accorde des privilèges insertion, mise à jour, suppression et sélection l'utilisateur admin_simpluedo : 
+--------------------------------------------------------------------------------------------------------
+
+GRANT INSERT, UPDATE, DELETE, SELECT ON TABLE roles, personnages, utilisateurs, salles, objets, visiter TO 'admin_simpluedo';
+
+--------------------------------------------------------------------------------------------------------
+-- Révoque les privilèges insertion, mise à jour, suppression et sélection : 
+--------------------------------------------------------------------------------------------------------
+
+REVOKE UPDATE ON TABLE roles, personnages, utilisateurs, salles, objets, visiter FROM PUBLIC;
 --------------------------------------------------------------------------------------------------------
 
 --> UUID PRIMARY KEY : permet de générer des identifiants uniques pour assurer une unicité globale. 
